8300b3233d2a89d948d02ea4a680c983d69ebe35
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-08-30 17:58:36 +0200

    Initial commit

---
+++ OGLU.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using SharpGL;
+using SharpGL.SceneGraph;
+
+namespace OpenGL_Util
+{
+    public interface IDrawable
+    {
+        void Draw(OpenGL gl, Vector3 offset);
+    }
+
+    public static class Extensions
+    {
+        public static Vertex Vertex(this Vector3 a) => new Vertex(a.X, a.Y, a.Z);
+        public static Vector3 Vector(this Vertex a) => new Vector3(a.X, a.Y, a.Z);
+
+        public static Vector3 IntCast(this Vector3 a) => new Vector3((int) a.X, (int) a.Y, (int) a.Z);
+
+        public static float Magnitude(this Vector3 a) => MathF.Sqrt(a.X * a.X + a.Y * a.Y + a.Z * a.Z);
+
+        public static Vector3 Normalize(this Vector3 a) => a / a.Magnitude();
+
+        public static Vector3 Forward(this Quaternion it) => new Vector3(
+            2 * (it.X * it.Z + it.W * it.Y),
+            2 * (it.Y * it.Z - it.W * it.X),
+            1 - 2 * (it.X * it.X + it.Y * it.Y)
+        );
+
+        public static Vector3 Up(this Quaternion it) => new Vector3(
+            2 * (it.X * it.Y - it.W * it.Z),
+            1 - 2 * (it.X * it.X + it.Z * it.Z),
+            2 * (it.Y * it.Z + it.W * it.X)
+        );
+
+        public static Vector3 Left(this Quaternion it) => new Vector3(
+            1 - 2 * (it.Y * it.Y + it.Z * it.Z),
+            2 * (it.X * it.Y + it.W * it.Z),
+            2 * (it.X * it.Z - it.W * it.Y)
+        );
+
+        public static Vector3 EulerAngles(this Quaternion q)
+        {
+            // https://stackoverflow.com/questions/11492299/quaternion-to-euler-angles-algorithm-how-to-convert-to-y-up-and-between-ha
+
+            // Store the Euler angles in radians
+            Vector3 pitchYawRoll = new Vector3();
+
+            double sqw = q.W * q.W;
+            double sqx = q.X * q.X;
+            double sqy = q.Y * q.Y;
+            double sqz = q.Z * q.Z;
+
+            // If quaternion is normalised the unit is one, otherwise it is the correction factor
+            double unit = sqx + sqy + sqz + sqw;
+            double test = q.X * q.Y + q.Z * q.W;
+
+            if (test > 0.4999f * unit) // 0.4999f OR 0.5f - EPSILON
+            {
+                // Singularity at north pole
+                pitchYawRoll.Y = 2f * (float) Math.Atan2(q.X, q.W); // Yaw
+                pitchYawRoll.X = MathF.PI * 0.5f; // Pitch
+                pitchYawRoll.Z = 0f; // Roll
+                return pitchYawRoll;
+            }
+            else if (test < -0.4999f * unit) // -0.4999f OR -0.5f + EPSILON
+            {
+                // Singularity at south pole
+                pitchYawRoll.Y = -2f * (float) Math.Atan2(q.X, q.W); // Yaw
+                pitchYawRoll.X = -MathF.PI * 0.5f; // Pitch
+                pitchYawRoll.Z = 0f; // Roll
+                return pitchYawRoll;
+            }
+            else
+            {
+                pitchYawRoll.Y = (float) Math.Atan2(2f * q.X * q.W + 2f * q.Y * q.Z, 1 - 2f * (sqz + sqw)); // Yaw 
+                pitchYawRoll.X = (float) Math.Asin(2f * (q.X * q.Z - q.W * q.Y)); // Pitch 
+                pitchYawRoll.Z = (float) Math.Atan2(2f * q.X * q.Y + 2f * q.Z * q.W, 1 - 2f * (sqy + sqz)); // Roll 
+            }
+
+            return pitchYawRoll;
+        }
+
+        public static Quaternion Rotation(this Vector3 a, float angle)
+        {
+            return new Quaternion(a.Normalize(), angle);
+        }
+    }
+}
---
+++ Shape/Cuboid.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using SharpGL;
+using SharpGL.Enumerations;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Assets;
+
+namespace OpenGL_Util.Shape
+{
+    public class Cuboid : IDrawable
+    {
+        private Vertex[][] _vertices;
+        private Vertex _pos;
+        private Vertex _scale;
+
+        public Cuboid(Vector3 position, Vector3 scale)
+        {
+            Position = position;
+            Scale = scale / 2;
+        }
+
+        public Vector3 Position
+        {
+            get => _pos.Vector();
+            set
+            {
+                _pos = value.Vertex();
+            }
+        }
+
+        public Vector3 Scale { get => _scale.Vector(); set => _scale = value.Vertex(); }
+        public Quaternion Rotation => Quaternion.Identity;
+
+        public unsafe void Draw(OpenGL gl, Vector3 offset)
+        {
+            gl.Color(0.7, 0.7, 0.7);
+
+            var position = _pos + offset.Vertex();
+            var scale = _scale;
+            float[] pa = position + scale;
+            float[] pb = position - scale;
+            fixed (float* ptA = pa)
+            fixed (float* ptB = pb)
+                _vertices = CalcAllVertices(GetSidesOrder(), ptA, ptB);
+            for (var i = 0; i < _vertices.Length; i++)
+            {
+                var side = SideOrder[i];
+                var quad = _vertices[i];
+                var texture = GetTexture(side);
+                gl.BindTexture(OpenGL.GL_TEXTURE_2D, texture.TextureName);
+                gl.ActiveTexture(texture.TextureName);
+                gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_NEAREST);
+                gl.Begin(BeginMode.Quads);
+                gl.TexCoord(1, 1);
+                gl.Vertex(quad[0]);
+                gl.TexCoord(0, 1);
+                gl.Vertex(quad[1]);
+                gl.TexCoord(0, 0);
+                gl.Vertex(quad[2]);
+                gl.TexCoord(1, 0);
+                gl.Vertex(quad[3]);
+                gl.End();
+            }
+        }
+
+        private unsafe Vertex[][] CalcAllVertices(Block.Side[] sides, float* pa, float* pb)
+        {
+            List<Vertex[]> list = new List<Vertex[]>();
+            for (var i = 0; i < sides.Length; i++)
+                list.Add(CalcVertices(sides[i], pa, pb));
+            return list.ToArray();
+        }
+
+        private unsafe Vertex[] CalcVertices(Block.Side side, float* pa, float* pb)
+        {
+            float* pax = pa + 0, pay = pa + 1, paz = pa + 2;
+            float* pbx = pb + 0, pby = pb + 1, pbz = pb + 2;
+            switch (side)
+            {
+                case Block.Side.TOP:
+                    return new[]
+                    {
+                        new Vertex(*pax, *pay, *pbz),
+                        new Vertex(*pbx, *pay, *pbz),
+                        new Vertex(*pbx, *pay, *paz),
+                        new Vertex(*pax, *pay, *paz)
+                    };
+                case Block.Side.BOTTOM:
+                    return new[]
+                    {
+                        new Vertex(*pax, *pby, *paz),
+                        new Vertex(*pbx, *pby, *paz),
+                        new Vertex(*pbx, *pby, *pbz),
+                        new Vertex(*pax, *pby, *pbz)
+                    };
+                case Block.Side.LEFT:
+                    return new[]
+                    {
+                        new Vertex(*pbx, *pay, *paz),
+                        new Vertex(*pbx, *pay, *pbz),
+                        new Vertex(*pbx, *pby, *pbz),
+                        new Vertex(*pbx, *pby, *paz)
+                    };
+                case Block.Side.RIGHT:
+                    return new[]
+                    {
+                        new Vertex(*pax, *pay, *pbz),
+                        new Vertex(*pax, *pay, *paz),
+                        new Vertex(*pax, *pby, *paz),
+                        new Vertex(*pax, *pby, *pbz)
+                    };
+                case Block.Side.FRONT:
+                    return new[]
+                    {
+                        new Vertex(*pax, *pay, *paz),
+                        new Vertex(*pbx, *pay, *paz),
+                        new Vertex(*pbx, *pby, *paz),
+                        new Vertex(*pax, *pby, *paz)
+                    };
+                case Block.Side.BACK:
+                    return new[]
+                    {
+                        new Vertex(*pax, *pby, *pbz),
+                        new Vertex(*pbx, *pby, *pbz),
+                        new Vertex(*pbx, *pay, *pbz),
+                        new Vertex(*pax, *pay, *pbz)
+                    };
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(side), side, "Invalid Side");
+            }
+        }
+    }
+}
