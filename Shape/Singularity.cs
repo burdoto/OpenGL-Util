ccb9159f3a4cb6fbe4d6121395ec706f149639ff
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-06 18:48:37 +0200

    Create Singularity.cs

---
+++ Shape/Singularity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace OpenGL_Util.Shape
+{
+    public struct Singularity : ITransform
+    {
+        public Singularity(Vector3 position) : this(position, Quaternion.Identity)
+        {
+        }
+
+        public Singularity(Vector3 position, Quaternion rotation) : this(position, rotation, Vector3.One)
+        {
+        }
+
+        public Singularity(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            Position = position;
+            Rotation = rotation;
+            Scale = scale;
+        }
+
+        public Vector3 Position { get; }
+        public Quaternion Rotation { get; }
+        public Vector3 Scale { get; }
+    }
+}
