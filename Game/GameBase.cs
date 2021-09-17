b2d642dd200a2a2c09e4373b3bef99230a7a199b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-17 13:55:07 +0200

    add tps test

--- Game/GameBase.cs
+++ Game/GameBase.cs
@@ -7,12 +7,14 @@ namespace OpenGL_Util.Game
 {
     public abstract class GameBase : Container, IDrawable
     {
+        public long TimeDelta { get; private set; }
         public bool Active { get; set; }
         public RenderMatrix RenderMatrix { get; } = new RenderMatrix();
-        
+
         public void Draw(OpenGL gl, ITransform camera)
         {
             RenderMatrix.Draw(gl, camera);
+            gl.DrawText(5, 20, 255, 0, 0, "Courier New", 12, $"Tick: {TimeDelta}ms");
         }
 
         public void Run()
@@ -22,8 +24,14 @@ namespace OpenGL_Util.Game
             if (!Enable())
                 throw new Exception("Could not enable game");
             Active = true;
+            long start;
             while (Active)
+            {
+                start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
                 Tick();
+                TimeDelta = DateTimeOffset.Now.ToUnixTimeMilliseconds() - start;
+            }
+
             Disable();
             Unload();
         }
