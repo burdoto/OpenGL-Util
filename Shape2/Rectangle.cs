0d5f61cb7143979ac72a1985e92a895b324c823c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-18 13:40:25 +0200

    fix rectangle rotation

--- Shape2/Rectangle.cs
+++ Shape2/Rectangle.cs
@@ -25,36 +25,21 @@ namespace OpenGL_Util.Shape2
 
         public override void Draw(OpenGL gl, ITransform camera)
         {
-            int radius = (int)Scale.X;
             //fixme
             gl.Begin(Filled ? BeginMode.TriangleFan : BeginMode.LineLoop);
             gl.Color(Color);
-            var vec = Position.Vertex();
-            var scale = Scale.Vertex();
-            var angle = Rotation.EulerAngles().Z;
-            var origin = GameObject.Transform.Position.Vertex();
-            // lol breakdown of the (false) quaternion effect
-            //scale = new Vertex(scale.X * MathF.Cos(angle) - scale.Y * MathF.Sin(angle), scale.X * MathF.Sin(angle) + scale.Y * MathF.Cos(angle), 0);
+            var mainPos = GameObject.Transform.Position;
+            var vec = Position - mainPos;
+            var scale = Scale;
             vec.X -= scale.X / 2;
-            vec = RotateAround(vec, origin, angle);
-            gl.Vertex(vec);
+            gl.Vertex((Vector3.Transform(vec, Rotation) + mainPos).Vertex());
             vec.X += scale.X;
-            vec = RotateAround(vec, origin, angle);
-            gl.Vertex(vec);
+            gl.Vertex((Vector3.Transform(vec, Rotation) + mainPos).Vertex());
             vec.Y += scale.Y;
-            vec = RotateAround(vec, origin, angle);
-            gl.Vertex(vec);
+            gl.Vertex((Vector3.Transform(vec, Rotation) + mainPos).Vertex());
             vec.X -= scale.X;
-            vec = RotateAround(vec, origin, angle);
-            gl.Vertex(vec);
+            gl.Vertex((Vector3.Transform(vec, Rotation) + mainPos).Vertex());
             gl.End();
-        }
-        
-        private static Vertex RotateAround(Vertex point, Vertex origin, float angle) {
-            angle = angle * MathF.PI / 180.0f;
-            return new Vertex(
-                MathF.Cos(angle) * (point.X - origin.X) - MathF.Sin(angle) * (point.Y - origin.Y) + origin.X,
-                y: MathF.Sin(angle) * (point.X - origin.X) + MathF.Cos(angle) * (point.Y - origin.Y) + origin.Y, 0);
         }
     }
 }
